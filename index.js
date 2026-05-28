474b4ea16b2dfd091f1058392e1a0462a2f33572
lluizz-dev <luiz.eduardodss24@gmail.com>
2026-05-28 20:52:43 -0300

    feat: add visual colors and grid layout

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const mosqueteiro = 1;
 const guarda = 2;
 
 function criarTabuleiro() {
+    const divTabuleiro = document.getElementById('tabuleiro');
+
     let tabuleiro = [
         [mosqueteiro, guarda, guarda,      guarda, guarda     ],
         [guarda,      guarda, guarda,      guarda, guarda     ],
@@ -12,4 +14,25 @@ function criarTabuleiro() {
         [guarda,      guarda, guarda,      guarda, guarda     ],
         [guarda,      guarda, guarda,      guarda, mosqueteiro],
     ];
+
+    for (let i = 0; i < tabuleiro.length; i++) {
+        for (let j = 0; j < tabuleiro[i].length; j++) {
+            let posicaoAtual = tabuleiro[i][j];
+            let casa = document.createElement('div');
+
+            if (posicaoAtual == vazio) {
+                casa.classList.add('casa', 'vazio');
+            }
+            else if (posicaoAtual == mosqueteiro) {
+                casa.classList.add('casa', 'mosqueteiro');
+            }
+            else {
+                casa.classList.add('casa', 'guarda');
+            }
+
+            divTabuleiro.appendChild(casa);
+        }
+    }
 }
+
+criarTabuleiro()
