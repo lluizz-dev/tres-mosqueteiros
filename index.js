11440a1f6514ccfb0ad34560b5c2a8d8c26422a2
lluizz-dev <luiz.eduardodss24@gmail.com>
2026-05-29 09:56:20 -0300

    feat: add event click on mosqueteiros

--- index.js
+++ index.js
@@ -3,11 +3,13 @@
 const vazio = 0
 const mosqueteiro = 1;
 const guarda = 2;
+let tabuleiro;
+const divTabuleiro = document.getElementById('tabuleiro');
+let vezAtual = mosqueteiro;
 
 function criarTabuleiro() {
-    const divTabuleiro = document.getElementById('tabuleiro');
 
-    let tabuleiro = [
+    tabuleiro = [
         [mosqueteiro, guarda, guarda,      guarda, guarda     ],
         [guarda,      guarda, guarda,      guarda, guarda     ],
         [guarda,      guarda, mosqueteiro, guarda, guarda     ],
@@ -30,15 +32,22 @@ function criarTabuleiro() {
                 casa.classList.add('casa', 'guarda');
             }
 
+            casa.addEventListener('click', function() {
+                movimentacao(i, j);
+            });
+
+            casa.dataset.linha = i;
+            casa.dataset.coluna = j;
+
             divTabuleiro.appendChild(casa);
         }
     }
 }
 
-function mensagemVez(vez) {
+function mensagemVez() {
     const h1MensagemVez = document.querySelector('#mensagem');
 
-    if (vez === mosqueteiro) {
+    if (vezAtual === mosqueteiro) {
         h1MensagemVez.textContent = "Vez do mosqueteiro!";
     }
     else {
@@ -46,5 +55,45 @@ function mensagemVez(vez) {
     }
 }
 
+function mudarVez(ultimaVez) {
+    if (ultimaVez === mosqueteiro) {
+        vezAtual = guarda;
+    }
+    else {
+        vezAtual = mosqueteiro;
+    }
+    mensagemVez();
+}
+
 criarTabuleiro();
-mensagemVez(mosqueteiro);
+mensagemVez();
+
+function movimentacao(i, j) {
+    let posicaoClique = tabuleiro[i][j];
+    let casa;
+
+    if (posicaoClique !== vezAtual) {
+        return;
+    }
+
+    if (posicaoClique === mosqueteiro) {
+        const direcoes = [
+            [-1, 0], // cima
+            [1, 0],  // baixo
+            [0, -1], // esquerda
+            [0, 1]   // direita
+        ];
+
+        for (const [di, dj] of direcoes) {
+            let novaLinha = i + di;
+            let novaColuna = j + dj;
+
+            if (novaLinha >= 0 && novaLinha < 5 && novaColuna >= 0 && novaColuna < 5) {
+                if (tabuleiro[novaLinha][novaColuna] === guarda) {
+                    casa = document.querySelector(`[data-linha="${novaLinha}"][data-coluna="${novaColuna}"]`);
+                    casa.classList.add('destacado');
+                }
+            }
+        }
+    }
+}
