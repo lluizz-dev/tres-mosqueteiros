d55de035d4f7ba30f498419049bd26b0921a485a
Luiz Eduardo <luiz.eduardodss24@gmail.com>
2026-07-24 15:28:22 -0300

    feat: init implements calcularMOvimentosValidos()

--- server/src/game/rules.ts
+++ server/src/game/rules.ts
@@ -1,8 +1,15 @@
-import type { Posicao } from "./types.js";
+import type { Posicao, Tabuleiro, Peca } from "./types.js";
 
 export function posicaoValida(posicao: Posicao): boolean {
     if (posicao.coluna < 0 || posicao.coluna > 4) return false;
     if (posicao.linha < 0 || posicao.linha > 4) return false;
 
     return true;
+}
+
+export function calcularMovimentosValidos(tabuleiro: Tabuleiro, origem: Posicao): Posicao[] {
+    if (posicaoValida(origem)) {
+        
+        const peca = tabuleiro[origem.linha]![origem.coluna]; 
+    }
 }
